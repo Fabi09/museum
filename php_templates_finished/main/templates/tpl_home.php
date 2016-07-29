ead79e1d38bc05075891085877bf6c9fdca99a75
SergiiShtun <sengru.90@gmail.com>
2016-07-29 15:27:31 +0200

    with Kim

---
+++ php_templates_finished/main/templates/tpl_home.php
@@ -0,0 +1,33 @@
+
+<div class="Text">
+
+	<h1> <?php echo  constant('SITE_'.strtoupper($_GET['site']).'_'.$_GET['language']);?> </h1><br />
+
+  <a>
+		<h1> Wilkommen </h1>
+		Mit der Buchreihe RessourcenKulturen entsteht ein Publikationsmedium für die Ergebnisse der Forschungen des von der Deutschen Forschungsgemeinschaft
+		geförderten Sonderforschungsbereich SFB 1070 RessourcenKulturen an der Eberhard-Karls-Universität Tübingen. Vorrangig wird dies Dissertationen,
+		andere monographische Schriften und Tagungsbände umfassen.
+		Zur Gewährleistung der Einhaltung allgemeiner Standards der Qualitätssicherung werden alle Bände einem internationalen Peer-Review-Verfahren unterzogen.
+		<br>
+		Mit ihren Bänden spiegelt die Reihe die Fachbreite und interdisziplinäre Kooperation des SFB wider, die aus Archäologien (Ur- und Frühgeschichte,
+		Archäologie des Mittelalters, Vorderasiatische Archäologie, Biblische Archäologie, Klassische Archäologie und Naturwissenschaftliche Archäologie),
+		Empirischer
+		Kulturwissenschaft, Ethnologie, Geographie, Geschichtswissenschaften und Historischen Philologien (Klassische Philologie, Vorderasiatische Philologie) besteht.
+		<br>
+		Um eine möglichst weite Verbreitung der Ergebnisse des SFB zu gewährleisten, ist neben des Drucks der Werke bewusst auch die Publikationsform des OpenAccess gewählt worden.
+		Die Bände sind über die Homepage des SFB 1070 sowie über die Website der Tübinger Universitätsbibliothek (https://publikationen.uni-tuebingen.de/) einzusehen.
+
+
+";
+  </a>
+
+
+
+
+
+
+
+
+
+</div>
