49e44668dbabca5c4cdb295fd95dbde95cb61980
SergiiShtun <sengru.90@gmail.com>
2016-07-29 15:02:09 +0200

    with Kim
    Homepage

---
+++ php_templates_finished/index.php
@@ -0,0 +1,107 @@
+<?php
+require_once ('main/config.php');				# Config-file
+require_once ('main/php/functions.php');        # PHP-functions
+
+init_get ('site',			'');
+init_get ('project',		'');
+#init_get ('subproject',	'');
+init_get ('language',		DEFAULT_LANGUAGE);
+?>
+<!DOCTYPE html>
+<html lang="de">
+	<head>
+    	<title><?php echo 'Ressourcen Kulturen:'.$_GET['site'] ?></title>
+
+    	<meta charset="UTF-8"/>
+    	<link href=main/stylesheets/style.css rel="stylesheet" type="text/css">
+  	</head>
+
+
+	<!------------Body---------------------->
+  	<body>
+
+
+		<!------------banner---------------------->
+			<div class="banner">
+				<img src="main/images/Logo-uni-tuebingen.png" >
+				<!--Deutsch/Englisch-->
+				<div id="flag"  align="right" >
+					<?php ECHO_FLAGS (); ?>
+				</div>
+			</div>
+
+
+	    <?php
+	 		require_once('main/templates/tpl_navibar.php'); ?>
+
+
+
+			<div class="timeline">
+
+					<img src="main/images/Zeitstrahl.jpg"  usemap="#timetable" alt="timetable" />
+
+					<map id="timetable" name="timetable">
+						<area shape="rect" alt="B01" title="" coords="102,196,171,214" href="" target="" />
+						<area shape="rect" alt="A01" title="" coords="332,85,624,105" href="" target="" />
+						<area shape="rect" alt="B01" title="" coords="482,255,1026,275" href="" target="" />
+						<area shape="rect" alt="A03" title="" coords="681,226,743,246" href="" target="" />
+						<area shape="rect" alt="A06" title="" coords="792,227,828,245" href="" target="" />
+						<area shape="rect" alt="A05" title="" coords="647,196,886,216" href="" target="" />
+						<area shape="rect" alt="C05" title="" coords="1121,194,1158,217" href="" target="" />
+						<area shape="rect" alt="C03" title="" coords="865,227,970,244" href="" target="" />
+						<area shape="rect" alt="A04" title="" coords="768,86,840,106" href="" target="" />
+						<area shape="rect" alt="B07" title="" coords="752,54,844,77" href="" target="" />
+						<area shape="rect" alt="B04" title="" coords="882,85,917,106" href="" target="" />
+						<area shape="rect" alt="C02" title="" coords="885,28,934,46" href="" target="" />
+						<area shape="rect" alt="B06" title="" coords="1056,85,1099,105" href="" target="" />
+						<area shape="rect" alt="B03" title="" coords="1110,56,1160,76" href="" target="" />
+						<area shape="rect" alt="C04" title="" coords="1184,86,1224,105" href="" target="" />
+						<area shape="rect" alt="C06" title="" coords="1187,196,1227,218" href="" target="" />
+
+					</map>
+
+				</div>
+
+<div class="Background">
+
+<?php
+			if (!empty ($_GET['site'])){
+				require_once ('main/templates/tpl_'.strtolower($_GET['site']).'.php');
+			}
+
+			else {
+				require_once ('main/templates/tpl_'.strtolower($_GET['project']).'.php');
+			}
+		?>
+
+	 	</div>
+
+
+
+		<!--------------Impressum---------------->
+		<div class="Impressum">
+
+				<div style="float: left; color: black; font-family: Open Sans; padding-top: 80px;">
+						University of Tübingen<br>
+						SFB 1070 ResourceCultures<br>
+						Gartenstraße 29 &middot; D-72074 Tübingen<br> <br>
+						Phone +49 7071 29-73596<br>
+						www.sfb1070.uni-tuebingen.de
+
+						</div>
+									<img style="float: right;" src="main/images/SFB1070-Logo.png" width="300px" height="300px">
+
+
+
+
+
+						</div>
+
+					</div>
+
+ 	</body>
+
+
+
+
+</html>
