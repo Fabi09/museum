b04a601a796b80c7cf93c9c096612c006cbbf034
SergiiShtun <sengru.90@gmail.com>
2016-07-29 15:25:53 +0200

    with Kim

---
+++ php_templates_finished/main/templates/tpl_c.php
@@ -0,0 +1,115 @@
+
+<div class="Text">
+
+	<h1> <?php echo  constant('SITE_PROJECT_'.strtoupper($_GET['project']).'_'.$_GET['language']);?> </h1><br />
+
+	<div class="read-more-box">
+		<h2 > C 02 ·  Bilder des Mangels und des Überflusses.  </br>
+		Ressourcen und ihre literarische Konstruktion im Kontext der „Griechischen
+		Kolonisation“
+		</h2>
+		<p>
+		<img class="img-left" src="main/images/C02.JPG" width="160px" height="160px" >
+
+		Das Teilprojekt untersucht die literarische Präsentation,
+		Einordnung und Bewertung agrarischer sowie sozialer Ressourcen in unterschiedlichen Räumen und Zeiten der griechischen Geschichte .
+		Es geht dabei um die Identifikation und Analyse der literarischen Verarbeitung von ressourcenbezogenen,
+		lebensweltlichen Erfahrungen im historischen Kontext der sogenannten
+		„großen griechischen Kolonisation“ des 8 . und 7 . Jh . v . Chr . sowie des Frühhellenismus (3 . Jh . v . Chr .) .   </p></br>
+		<a class="read-more-btn" href="index2.php?projekt=c02"> Mehr lesen </a>
+	</div>
+
+		<div class="read-more-box">
+	    <h2 > C 03 · Ressourcen und die Formierung von  Gesellschaften, Siedlungsräumen und
+		kulturellen Identitäten der italischen Halbinsel im ersten  vorchristlichen Jahrtausend
+	</h2>
+	   <p><img class="img-left" src="main/images/C03.jpg" width="160px" height="160px"  >
+
+
+
+		Das Konzept des SFB 1070 Ressourcen als Grundlage der Bildung und des Wandels sozialer Gruppen zu sehen,
+		wird in diesem Teilprojekt genutzt, um ein neues Verständnis der Zusammenhänge von Besiedlungsprozessen und
+		der Formierung von Gesellschaften und kulturellen Identitäten auf der italischen Halbinsel
+		des ersten Jahrtausends v . Chr . zu gewinnen . RessourcenKomplexe um Metalle und um landwirtschaftlich
+		nutzbaren Boden in Siedlungsräumen an dem von etruskischen
+		Städten dominierten Tyrrhenischen Meer und der „Magna Graecia“ werden vergleichend betrachtet .  </p></br>
+		<a class="read-more-btn" href="index2.php?projekt=c03"> Mehr lesen </a>
+		</div>
+
+
+
+		   <div class="read-more-box">
+	    <h2 > C 04 · Religiöse Ressourcen: Wertschöpfung und  Wertkonvertierung von Ressourcen in Zentral- und Südasien
+
+	 </h2>
+	   <p><img class="img-left" src="main/images/C04.JPG" width="160px" height="160px"  >
+
+
+
+		Das Teilprojekt behandelt die Rolle von RessourcenKomplexen für die Bildung, den Erhalt und die Vergrößerung von
+		bedeutenden religiösen Institutionen in Süd- und Zentralasien . Im Rahmen von Feldforschungen in Indien, Kyrgyzstan und Iran
+		sollen ethnographische und ethnohistorische Daten zu religiösen Ressourcen erhoben werden . Die Fallbeispiele werden sozio-kulturelle
+		Praktiken der Wertschöpfung und Wertkonvertierung religiöser Ressourcen aufzeigen,
+		Verflechtungen zwischen Religion, Ökonomie und Staat identifizieren und vermitteln, wie Konflikte
+		um religiöse Ressourcen entstehen .</p></br>
+		<a class="read-more-btn" href="index2.php?projekt=c04"> Mehr lesen </a>
+		</div>
+
+
+		   <div class="read-more-box">
+	    <h2 > C 05 · Profit und Verschwendung von Ressourcen.  </br>Die Schaffung von ideologischem Kapital während der  Wikingerzeit und im Hochmittelalter in Nordeuropa
+
+	 </h2>
+	   <p><img class="img-left" src="main/images/C05.JPG" width="160px" height="160px"  >
+
+
+
+		Im Fokus der Untersuchung steht die Verschwendung bzw . Investition in profitorientiertes oder
+		ideologisches Kapital am Beispiel der mittelalterlichen Depotfunde und Kirchenbauten der Ostseeinsel Gotland .
+		Bei den Schatzfunden wird der Fragmentierungsgrad, die Ausführungsqualität, ikonographische Bedeutung und Herkunft
+		der Objekte aus anderen Wirtschaftsräumen analysiert. Im Fokus der Komplementärstudie stehen die Investitionsvolumen für den Bau,
+		die Ausstattung und die Ausschmückung der Kirchen, sowohl .....</p></br>
+		<a class="read-more-btn" href="index2.php?projekt=c05"> Mehr lesen </a>
+		</div>
+
+		   <div class="read-more-box">
+	    <h2 > C 06 · Reis und Heilpflanzen. </br>Die kulturelle Konstruktion nährender und heilender Pflanzen in Südindien
+
+	 </h2>
+	   <p><img class="img-left" src="main/images/C06.png" width="160px" height="160px"  >
+
+
+
+		Das Projekt beschäftigt sich mit den südindischen Ressourcen Reis und Heilpflanzen .
+		Reis bildet die hauptsächliche Anbaupflanze in den Schwemmgebieten der südostindischen Flussdeltaregion,
+		Heilpflanzen wachsen vor allem in den Wäldern der Bergregionen Südindiens . In der ersten Untersuchung soll aufgezeigt werden,
+		wie die Reiswirtschaft und die Produkte Paddy und Reis in spezifische, historisch gewachsene Bedeutungsmuster und soziale Strukturen
+		eingewoben sind .
+	  </p></br>
+		<a class="read-more-btn" href="index2.php?projekt=c06"> Mehr lesen </a>
+		</div>
+
+		   <div class="read-more-box">
+	    <h2 > C 07 · Prähistorische Ikonen als Ressourcen  in  Vergangenheit und Gegenwart
+
+	 </h2>
+	   <p><img class="img-left" src="main/images/C07.jpg" width="160px" height="160px"  >
+
+
+
+		Die kulturelle Ressource „Ikone“ stellt Teil eines komplexen Prozesses dar und muss aus einer diachronen
+		Perspektive analysiert werden . Für die erste Phase des Projekts ist eine Studie, sowohl der Schiffsgräber von Oseberg,
+		Gokstad und Hedeby, als auch des Monuments von Jelling geplant . Die Bedeutung der Monumente aus identitäts- und nationsstiftenden
+		Gründen ist bisher unerforscht. Mittels der kombinierten Untersuchung zur Bedeutung dieser Ikonen in Vergangenheit und Gegenwart soll aufgezeigt werden,
+		dass die scheinbare archäologische Objektivität  .....
+		   </p></br>
+		<a class="read-more-btn" href="index2.php?projekt=c07"> Mehr lesen </a>
+		</div>
+
+
+
+
+
+
+
+</div>
