5f7ae70f3b01f4e4527e34bc9c42e8c001936c4c
SergiiShtun <sengru.90@gmail.com>
2016-07-29 15:07:34 +0200

    Add files via upload

---
+++ php_templates_finished/main/config.php
@@ -0,0 +1,71 @@
+<?php
+
+# Default values for $_GET-vars
+define ('DEFAULT_CONTENT',  'Startseite');
+define ('DEFAULT_LANGUAGE', 'DE');
+
+
+# language
+define ('LANGUAGE_DE', 'DE');
+define ('LANGUAGE_EN', 'EN');
+
+
+
+
+# Images
+define ('IMG_GLOBE',			'main/images/globe-green.png');
+define ('IMG_FLAG_DE',    'main/images/de.gif');
+define ('IMG_FLAG_EN',    'main/images/en.gif');
+
+# Title
+define ('TITLE_DE', 'SFB 1070 RessourcenKulturen');
+define ('TITLE_EN', 'SFB 1070 ResourceCultures');
+
+# Language
+define ('LBL_LANGUAGE_DE', 'Deutsch');
+define ('LBL_LANGUAGE_EN', 'English');
+
+define ('SITE_HOME_DE', 'Home');
+define ('SITE_HOME_EN', 'Home');
+
+define ('SITE_PROJECTS_DE', 'Teilprojekte');
+define ('SITE_PROJECTS_EN', 'Projects');
+
+define ('SITE_PROJECT_A_DE', 'Entwicklungen');
+define ('SITE_PROJECT_A_EN', 'Developments');
+
+define ('SITE_PROJECT_B_DE', 'Bewegungen');
+define ('SITE_PROJECT_B_EN', 'Movements');
+
+define ('SITE_PROJECT_C_DE', 'Bewertungen');
+define ('SITE_PROJECT_C_EN', 'Valuations');
+
+define ('SITE_PROJECT_M_DE', 'Minigraduiertenkolleg');
+define ('SITE_PROJECT_M_EN', 'Special Research Unit');
+
+define ('SITE_HISTORY_DE', 'Geschichte');
+define ('SITE_HISTORY_EN', 'History');
+
+define ('SITE_PUBLICATIONS_DE', 'Publikationen');
+define ('SITE_PUBLICATIONS_EN', 'Publications');
+
+define ('SITE_GALLERY_DE', 'Galerie');
+define ('SITE_GALLERY_EN', 'Gallery');
+
+define ('SITE_GLOBE_DE', 'Karte');
+define ('SITE_GLOBE_EN', 'Map');
+
+# Projects
+define ('PROJECT_A_DE', 'A: Entwicklungen');
+define ('PROJECT_A_EN', 'A: Developments');
+
+define ('PROJECT_B_DE', 'B: Bewegungen');
+define ('PROJECT_B_EN', 'B: Movements');
+
+define ('PROJECT_C_DE', 'C: Bewertungen');
+define ('PROJECT_C_EN', 'C: Valuations');
+
+define ('PROJECT_M_DE', 'Mini-Graduiertenkolleg');
+define ('PROJECT_M_EN', 'Special Research Unit');
+
+?>
