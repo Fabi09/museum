53fd0a24201b5093aab92622f45627eef5ed0cf1
SergiiShtun <sengru.90@gmail.com>
2016-07-29 15:24:31 +0200

    with Kim

---
+++ php_templates_finished/main/templates/tpl_a.php
@@ -0,0 +1,118 @@
+
+<div class="Text">
+
+	<h1> <?php echo  constant('SITE_PROJECT_'.strtoupper($_GET['project']).'_'.$_GET['language']);?> </h1><br />
+
+
+  <div class="read-more-box">
+  	<h2 > A 01  ·   Ressourcen und die Herausbildung von Ungleichheit. Rohstoffe und Kommunikationssysteme im prähistorischen Südosteuropa
+
+  	</h2>
+  	<p>
+  	<img class="img-left" src="main/images/A01.JPG" width="160px" height="160px" >
+
+  	Das Ziel des Projektes ist es, Veränderungen der Nutzung von anstehenden Ressourcen im
+  	prähistorischen Südosteuropa anhand von zwei  Beispielregionen, den Gebieten des Banat
+  	in Südwestrumänien und dem bulgarischen Schwarzmeerraum, zu untersuchen . Die vergleichende Analyse wird
+  	insbesondere soziale Auswirkungen auf die Bevölkerung im
+  	Zusammenhang mit der Ressourcennutzung aufzeigen . </p></br>
+  	<a class="read-more-btn" href="index2.php?projekt=a01"> Mehr lesen </a>
+  </div>
+
+  	<div class="read-more-box">
+      <h2 > A 02 · Viel Erz und wenig Wasser. Sozio-kultureller  Wandel in Verbindung mit
+  	Ressourcennutzung in der jüngeren  Vorgeschichte der iberischen Halbinsel
+
+  </h2>
+     <p><img class="img-left" src="main/images/A02.jpg" width="160px" height="160px"  >
+
+
+
+  	In diesem Teilprojekt werden die Dynamiken und
+  	die Diversität der sozio-kulturellen Erscheinungsformen auf der
+  	iberischen Halbinsel in Relation zur Nutzung von Ressourcen umfassend in einer
+  	Langzeitperspektive vom dritten bis ersten Jahrtausend v . Chr . analysiert und rekonstruiert .
+  	Durch die geplanten Gegenüberstellungen detaillierter Studien in zwei Regionen mit deutlich verschiedenen
+  	Standortbedingungen ergibt sich die Chance Ressourcen als Faktoren in solchen Prozessen wesentlich deutlicher als bisher zu
+  	identifizieren .
+    </p></br>
+  	<a class="read-more-btn" href="index2.php?projekt=a02"> Mehr lesen </a>
+  	</div>
+
+
+
+  	   <div class="read-more-box">
+      <h2 > A 03 ·  Steine aus dem Süden. Der Austausch von  Ressourcen zwischen Mesopotamien und dem Gebiet  des Persischen Golfs
+
+
+   </h2>
+     <p><img class="img-left" src="main/images/A03.jpg" width="160px" height="160px"  >
+
+
+
+  	In dem Teilprojekt soll die Zirkulation von Ressourcen im Bereich
+  	des Persischen Golfes, der vor allem auf den Erhalt wertvoller Steine durch die
+  	mesopotamischen Stadtstaaten und Großreiche des 3 . Jahrtausends v . Chr . abzielte, erforscht werden .
+  	Im Zentrum stehen dabei die Auswirkungen dieser weiträumigen Prozesse auf die Herausbildung von lokalen RessourcenKulturen
+  	in der Golfregion . Zu diesem Zweck wird in Zusammenarbeit mit dem Iranischen Forschungsinstitut für Kulturerbe und
+  	Tourismus (richt) ein archäologischer Survey in der
+  	iranischen Provinz Kerman durchgeführt....</p></br>
+  	<a class="read-more-btn" href="index2.php?projekt=a03"> Mehr lesen </a>
+  	</div>
+
+
+  	   <div class="read-more-box">
+      <h2 > A 04  · Die Entwicklung der Palast-RessourcenKulturen Syriens</h2>
+     <p><img class="img-left" src="main/images/A04.jpg" width="160px" height="160px"  >
+
+
+
+  	Das Teilprojekt zielt darauf ab, das traditionelle Konzept der
+  	Palastwirtschaft für die syrischen Klein- und Mittelstaaten des 2 . Jahrtausends v . Chr .
+  	durch ein Modell der Palast- RessourcenKulturen zu ersetzen und die Entwicklung dieser Systeme zu erforschen .
+  	Dieses neue Konzept stellt die gezielte Belegung von Materialien und Objekten
+  	mit kulturellem Symbolgehalt zu Zwecken der Herrschaftsstabilisierung in den Vordergrund .
+  	In dem Teilprojekt wird die Bedeutung von Gütern aus wertvollen Materialien in palatialen Kontexten untersucht .
+   </p></br>
+  	<a class="read-more-btn" href="index2.php?projekt=a04"> Mehr lesen </a>
+  	</div>
+
+  	   <div class="read-more-box">
+      <h2 > A 05  · Reis und Heilpflanzen. </br> „Das Land, in dem Milch und Honig fließen“.  Entwicklung und Bedeutung von Agrarressourcen im  bronze- und eisenzeitlichen Palästina
+
+
+   </h2>
+     <p><img class="img-left" src="main/images/A05.JPG" width="160px" height="160px"  >
+
+
+
+  	Das Teilprojekt soll die landwirtschaftlichen Ressourcen
+  	Palästinas mit Schwerpunkt auf dem Ackerbau durch zwei Fallstudien
+  	erforschen: eine archäobotanische Fallstudie, welche die naturwissenschaftlichen
+  	und ethnologischen Quellen untersuchen wird, und eine biblisch archäologische Fallstudie
+  	zu den archäologischen, historischen und philologischen Quellen . Das Arbeitsprogramm wird dabei
+  	so strukturiert werden, dass eine möglichst enge Kooperation zwischen beiden Fallstudien erzielt werden kann .
+
+    </p></br>
+  	<a class="read-more-btn" href="index2.php?projekt=a05"> Mehr lesen </a>
+  	</div>
+
+  	   <div class="read-more-box">
+      <h2 > A 06  · Politischer Kollaps als Folge ökonomischen Wandels? Ressourcenkontrolle
+  	am Übergang von der Bronze- zur  Eisenzeit im Ostmittelmeerraum
+   </h2>
+     <p><img class="img-left" src="main/images/A06.JPG" width="160px" height="160px"  >
+
+
+
+  	Das Teilprojekt betrachtet am Beispiel des Übergangs von der Bronze- zur Eisenzeit
+  	im Ostmittelmeerraum die Frage nach den Wechselwirkungen zwischen sozio-kulturellen Dynamiken
+  	und der Nutzung  von Ressourcen . Während der ersten Phase des SFB sollen in einer Fallstudie archäologische
+  	Funde der süd lichen Levante aus dem obengenannten Blickwinkel untersucht werden . In späteren Phasen des SFB werden
+  	weitere Fallstudien das Untersuchungsgebiet ausweiten um zu einem besseren Verständnis der wirtschaftlichen,
+  	politischen und historischen Vorgänge während ....
+  	   </p></br>
+  	<a class="read-more-btn" href="index2.php?projekt=a06"> Mehr lesen </a>
+  	</div>
+
+</div>
