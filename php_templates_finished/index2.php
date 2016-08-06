5455b6d347063262df3f83ed3a4ba3f005a69388
SergiiShtun <sengru.90@gmail.com>
2016-08-06 13:30:25 +0200

    Update index2.php

--- php_templates_finished/index2.php
+++ php_templates_finished/index2.php
@@ -31,31 +31,7 @@ init_get ('language',		DEFAULT_LANGUAGE);
 			</div>
 
 
-		    <?php
-		 		require_once('main/templates/tpl_navibar.php'); ?>
-
-				<div class="timeline">
-						<img src="main/images/timetable.jpg"  usemap="#timetable" alt="timetable" width="100%"/>
-						<map id="timetable" name="timetable">
-							<area shape="rect" alt="A01" title="A01" coords="308,80,588,91" href="" target="_blank" />
-							<area shape="rect" alt="B01" title="B01" coords="106,185,178,199" href="" target="" />
-							<area shape="rect" alt="B01" title="B01" coords="458,240,983,259" href="" target="" />
-							<area shape="rect" alt="A03" title="A03" coords="648,213,702,229" href="" target="" />
-							<area shape="rect" alt="A06" title="A06" coords="756,214,786,227" href="" target="" />
-							<area shape="rect" alt="C03" title="C03" coords="827,210,915,228" href="" target="" />
-							<area shape="rect" alt="A05" title="A05" coords="614,184,847,203" href="" target="" />
-							<area shape="rect" alt="A04" title="A04" coords="729,82,801,93" href="" target="" />
-							<area shape="rect" alt="B04" title="B04" coords="839,78,873,96" href="" target="" />
-							<area shape="rect" alt="B07" title="B07" coords="722,50,807,62" href="" target="" />
-							<area shape="rect" alt="C02" title="C02" coords="845,23,886,37" href="" target="" />
-							<area shape="rect" alt="B06" title="B06" coords="1009,77,1052,96" href="" target="" />
-							<area shape="rect" alt="B03" title="B03" coords="1061,48,1101,67" href="" target="" />
-							<area shape="rect" alt="C04" title="C04" coords="1120,78,1173,98" href="c04.html" target="" />
-							<area shape="rect" alt="C05" title="C05" coords="1070,185,1109,201" href="" target="" />
-							<area shape="rect" alt="C06" title="C06" coords="1134,183,1169,200" href="c06.html" target="" />
-						</map>
-					</div>
-
+		    <?php require_once('main/templates/tpl_navibar.php'); ?>
 
 
 <!---------Background------------------>
