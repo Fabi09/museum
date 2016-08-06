f45a42b8385527e17f3de66845317534687ca8fb
SergiiShtun <sengru.90@gmail.com>
2016-08-06 14:53:35 +0200

    Update tpl_home.php

--- php_templates_finished/main/templates/tpl_home.php
+++ php_templates_finished/main/templates/tpl_home.php
@@ -1,33 +1,69 @@
-
 <div class="Text">
 
-	<h1> <?php echo  constant('SITE_'.strtoupper($_GET['site']).'_'.$_GET['language']);?> </h1><br />
+<?php
+$directory = realpath('main/data/'.$_GET['site'].'/');
+foreach (RETURN_CSVFILE_CONTENT (realpath($directory.'/inhalt_'.($_GET['language']).'.csv')) as $key_index => $array){
+if(!empty($array['titel'])){
+echo <<<EOT
 
-  <a>
-		<h1> Wilkommen </h1>
-		Mit der Buchreihe RessourcenKulturen entsteht ein Publikationsmedium für die Ergebnisse der Forschungen des von der Deutschen Forschungsgemeinschaft
-		geförderten Sonderforschungsbereich SFB 1070 RessourcenKulturen an der Eberhard-Karls-Universität Tübingen. Vorrangig wird dies Dissertationen,
-		andere monographische Schriften und Tagungsbände umfassen.
-		Zur Gewährleistung der Einhaltung allgemeiner Standards der Qualitätssicherung werden alle Bände einem internationalen Peer-Review-Verfahren unterzogen.
-		<br>
-		Mit ihren Bänden spiegelt die Reihe die Fachbreite und interdisziplinäre Kooperation des SFB wider, die aus Archäologien (Ur- und Frühgeschichte,
-		Archäologie des Mittelalters, Vorderasiatische Archäologie, Biblische Archäologie, Klassische Archäologie und Naturwissenschaftliche Archäologie),
-		Empirischer
-		Kulturwissenschaft, Ethnologie, Geographie, Geschichtswissenschaften und Historischen Philologien (Klassische Philologie, Vorderasiatische Philologie) besteht.
-		<br>
-		Um eine möglichst weite Verbreitung der Ergebnisse des SFB zu gewährleisten, ist neben des Drucks der Werke bewusst auch die Publikationsform des OpenAccess gewählt worden.
-		Die Bände sind über die Homepage des SFB 1070 sowie über die Website der Tübinger Universitätsbibliothek (https://publikationen.uni-tuebingen.de/) einzusehen.
+<h1>{$array['titel']}</h1>
 
+<span>{$array['zusammenfassung']}</span>
+<br/>
+<br/>
+EOT;
+}
+}
+foreach (RETURN_CSVFILE_CONTENT (realpath($directory.'/newsfeed_'.($_GET['language']).'.csv')) as $key_index => $array){
+if(!empty($array['tag'])){
+							echo <<<EOT
 
-";
-  </a>
+								<span>{$array['tag']}-{$array['monat']}-{$array['jahr']}:</span>
+								<br>
+								<span>{$array['beschreibung']}</span>
+                <br/>
+EOT;
+}
+}
+?>
+</div>
 
+					<div align="center" >
+					<iframe width="920" height="500" src="https://www.youtube.com/embed/FR3Zhw8KqT0?rel=0" frameborder="0" allowfullscreen></iframe>
+					</div>
 
+<!--------------Bildermenu-------------------->
 
+        <div class="Images">
 
+          <img src="main/images/A.jpg"   width="400" height="340">
+          <a href="#">
+          <spanA>
+            Projekt A
+          </spanA></a>
+        </div>
 
+        <div class="Images">
+          <img src="main/images/B.jpg" width="400" height="340">
+          <a href="#">
+          <spanB>
+            Projekt B
+          </spanB></a>
+        </div>
 
+        <div class="Images">
+          <img src="main/images/C.jpg" width="400" height="340">
+          <a href="bewertungen.html">
+          <spanC>
+            Projekt C
+          </spanC></a>
+        </div>
 
-
-
+        <div class="Images">
+          <img src="main/images/mini.png" width="400" height="340">
+          <a href="#">
+          <Minigrad>
+            Minigraduiertenkolleg
+          </Minigrad></a>
+        </div>
 </div>
