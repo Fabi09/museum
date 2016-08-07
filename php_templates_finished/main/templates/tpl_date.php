d9eae9815796649e83c691aeed1ed3fa8ba35dee
georgkaza <georgios.kazantzidis@student.uni-tuebingen.de>
2016-08-07 22:09:28 +0200

    Update tpl_date.php

--- php_templates_finished/main/templates/tpl_date.php
+++ php_templates_finished/main/templates/tpl_date.php
@@ -1,10 +1,27 @@
 <div class="Text">
+<?php
 
-<h1> <?php echo  constant('SITE_'.strtoupper($_GET['site']).'_'.$_GET['language']);?> </h1><br />
+echo <<<EOT
+<h1>Termine</h1>
+EOT;
 
-<a>
-  Hallo
-</a>
-
-
+$directory = realpath('main/data/'.$_GET['site'].'/');
+foreach (RETURN_CSVFILE_CONTENT (realpath($directory.'/inhalt_'.($_GET['language']).'.csv')) as $key_index => $array){
+if(!empty($array['tag'])){
+echo '<tr>';
+				echo '<td align="left" style="width: 1%" valign="top">';
+						echo $array['tag'];
+						echo '-';
+						echo sprintf($array['monat']);
+						echo '-';
+						echo sprintf($array['jahr']);
+						echo ' ';
+						echo $array['beschreibung'];
+						echo '<br>';						
+				echo '</td>';
+echo '</tr>';
+			
+}
+}
+?>
 </div>
